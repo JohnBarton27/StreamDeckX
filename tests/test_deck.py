b6058635260756d630c0acb53a505b66f3dafe13
John Barton <john@mediayoucanfeel.com>
2021-06-29 18:01:29 -0400

    Fix unit test patching to actually mock out the StreamDeck DeviceManager

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -7,20 +7,23 @@ from streamdeckx.deck import Deck, XLDeck
 class TestDeck(unittest.TestCase):
 
     def setUp(self):
-        enumerate_patch = patch('streamdeckx.deck.DeviceManager.enumerate')
-        self.m_enumerate = enumerate_patch.start()
-        self.addCleanup(enumerate_patch.stop)
+        device_manager_patch = patch('streamdeckx.deck.DeviceManager')
+        m_dev_manager = device_manager_patch.start()
+        self.addCleanup(device_manager_patch.stop)
+
+        self.m_dev_manager = MagicMock()
+        m_dev_manager.return_value = self.m_dev_manager
 
     def test_get_connected_single_xl(self):
         """Deck.get_connected.single_xl"""
         xl_deck1 = MagicMock()
         xl_deck1.deck_type.return_value = 'Stream Deck XL'
         xl_deck1.id = 'xl_deck1_id'
-        self.m_enumerate.return_value = [xl_deck1]
+        self.m_dev_manager.enumerate.return_value = [xl_deck1]
         
         decks = Deck.get_connected()
 
-        self.m_enumerate.assert_called()
+        self.m_dev_manager.enumerate.assert_called()
 
         self.assertEqual(len(decks), 1)
         self.assertEqual(type(decks[0]), XLDeck)
@@ -32,11 +35,12 @@ class TestDeck(unittest.TestCase):
         unknown_deck1 = MagicMock()
         unknown_deck1.deck_type.return_value = 'Stream Deck Unknown'
         unknown_deck1.id = 'unknown_deck1_id'
-        self.m_enumerate.return_value = [unknown_deck1]
+        dev_manager = MagicMock()
+        self.m_dev_manager.enumerate.return_value = [unknown_deck1]
 
         decks = Deck.get_connected()
 
-        self.m_enumerate.assert_called()
+        self.m_dev_manager.enumerate.assert_called()
 
         self.assertEqual(len(decks), 0)
         m_print.assert_called()
