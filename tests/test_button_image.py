73bdc1832b33668303b4f40b8264c5647cfec9f8
John Barton <john@mediayoucanfeel.com>
2021-11-03 21:20:18 -0400

    SDX-6 Add setUp to TestButtonImage to globally apply 'get_text_dimension' override

--- tests/test_button_image.py
+++ tests/test_button_image.py
@@ -6,33 +6,30 @@ from button_image import ButtonImage
 
 class TestButtonImage(unittest.TestCase):
 
-    @patch('button_image.ButtonImage.get_text_dimensions')
-    def test_get_max_width_single(self, m_get_text_dimensions):
-        font = MagicMock()
+    def setUp(self) -> None:
+        get_text_dimensions_patch = patch('button_image.ButtonImage.get_text_dimensions')
+        self.m_get_text_dimensions = get_text_dimensions_patch.start()
+        self.addCleanup(get_text_dimensions_patch.stop)
+        self.m_get_text_dimensions.side_effect = get_text_dimensions_side_effect
 
-        m_get_text_dimensions.side_effect = get_text_dimensions_side_effect
+    def test_get_max_width_single(self):
+        font = MagicMock()
 
         text_lines = ['abc123']
         max_width = ButtonImage.get_max_width(text_lines, font)
 
         self.assertEqual(max_width, 6)
 
-    @patch('button_image.ButtonImage.get_text_dimensions')
-    def test_get_max_width_multiple(self, m_get_text_dimensions):
+    def test_get_max_width_multiple(self):
         font = MagicMock()
-
-        m_get_text_dimensions.side_effect = get_text_dimensions_side_effect
 
         text_lines = ['abc123', 'a', '456']
         max_width = ButtonImage.get_max_width(text_lines, font)
 
         self.assertEqual(max_width, 6)
 
-    @patch('button_image.ButtonImage.get_text_dimensions')
-    def test_get_max_width_multiple_in_middle(self, m_get_text_dimensions):
+    def test_get_max_width_multiple_in_middle(self):
         font = MagicMock()
-
-        m_get_text_dimensions.side_effect = get_text_dimensions_side_effect
 
         text_lines = ['abc123', 'abcdef1234', '456']
         max_width = ButtonImage.get_max_width(text_lines, font)
