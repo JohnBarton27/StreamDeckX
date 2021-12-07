5d1237eaa691f84020d5cdcbd740d837efcaf697
John Barton <john@mediayoucanfeel.com>
2021-12-06 19:01:54 -0500

    SDX-13 Move TextAction unit tests to separate file to match new class structure

---
+++ tests/test_action/test_text_action.py
@@ -0,0 +1,66 @@
+import unittest
+from unittest.mock import MagicMock
+
+from test_base import BaseStreamdeckXTest
+
+from action.text_action import TextAction
+
+
+class TestTextAction(BaseStreamdeckXTest):
+
+    def test_init(self):
+        """TextAction.__init__"""
+        btn = MagicMock()
+        action = TextAction('username', btn, 1)
+        self.assertEqual(action.text, 'username')
+
+    def test_repr(self):
+        """TextAction.__repr__"""
+        btn = MagicMock()
+        action = TextAction('username', btn, 1)
+        self.assertEqual(repr(action), 'username')
+
+    def test_str(self):
+        """TextAction.__str__"""
+        btn = MagicMock()
+        action = TextAction('username', btn, 1)
+        self.assertEqual(str(action), 'username')
+
+    def test_eq_equal(self):
+        """TextAction.__eq__.equal"""
+        btn = MagicMock()
+        action1 = TextAction('username', btn, 1)
+        action2 = TextAction('username', btn, 1)
+
+        self.assertEqual(action1, action2)
+
+    def test_eq_diff_types(self):
+        """TextAction.__eq__.diff_types"""
+        btn = MagicMock()
+        action1 = TextAction('username', btn, 1)
+        action2 = 'username'
+
+        self.assertNotEqual(action1, action2)
+
+    def test_eq_diff_text(self):
+        """TextAction.__eq__.diff_text"""
+        btn = MagicMock()
+        action1 = TextAction('username', btn, 1)
+        action2 = TextAction('password', btn, 1)
+
+        self.assertNotEqual(action1, action2)
+
+    def test_hash(self):
+        """TextAction.__hash__"""
+        btn = MagicMock()
+        action = TextAction('username', btn, 1)
+        self.assertEqual(hash(action), hash('username'))
+
+    def test_display_value(self):
+        btn = MagicMock()
+        action = TextAction('username', btn, 1)
+        self.assertEqual('username', action.display_value)
+
+
+if __name__ == '__main__':
+    unittest.main()
